1739da32587a36f2dc8fa57b43f9cef4bbfc80e3
Remco Stoeten <remcostoeten@hotmail.com>
2025-03-18 10:26:32 +0100

    add github link

--- src/app/modules/page.tsx
+++ src/app/modules/page.tsx
@@ -1,5 +1,17 @@
+import Link from "next/link"
 import ExampleFileTree from "./data"
 
 export default function Page() {
-    return <ExampleFileTree />
+    return (
+        <>
+            <ExampleFileTree />
+            <Link
+                className='text-sm text-gray-500 hover:text-gray-700 underline'
+                href='https://github.com/remcostoeten/Beautiful-interactive-file-tree/tree/boilerplate/src/app/modules'
+                target='_blank'
+            >
+                View source code on GitHub
+            </Link>
+        </>
+    )
 }
